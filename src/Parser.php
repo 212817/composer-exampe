9c79dce89c534d9ec77f53b25eb25c846d8741a1
vagrant <vagrant@images>
2018-01-20 19:28:56 +0000

    small changes

--- src/Parser.php
+++ src/Parser.php
@@ -17,6 +17,8 @@ class Parser implements ParserInterface
             return ['Invalid URL'];
         }
         
+        // new  комментарий 
+        
         // Regular expression
         preg_match_all('/<' . $tag . '.*?>(.*?)<\/' . $tag . '>/s', $htmlPage, $strings);
         if (empty($strings[1])) {
